af8314ceb7c876c7c33e069b843cbcea2a2e3cb7
urshita20 <135149929+urshita20@users.noreply.github.com>
2026-02-17 14:42:24 +0530

    Create careers.ts

---
+++ src/api/careers.ts
@@ -0,0 +1,12 @@
+export async function fetchCareers() {
+  try {
+    const response = await fetch(
+      "https://career-compass-backend.onrender.com/api/careers"
+    );
+
+    return await response.json();
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+}
